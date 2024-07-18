99409cdf8788733e598c0cad49bf34cfda01b586
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-07-18 19:07:06 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-4o-2024-05-13

---
+++ broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/Categories.java
@@ -0,0 +1,81 @@
+package pt.ulisboa.tecnico.softeng.broker.domain;
+
+public interface Categories {
+
+	interface getNumberOfAdventures {
+
+	}
+
+	interface roostTest1 {
+
+	}
+
+	interface roostTest2 {
+
+	}
+
+	interface addAdventure {
+
+	}
+
+	interface hasAdventure {
+
+	}
+
+	interface getID {
+
+	}
+
+	interface getBroker {
+
+	}
+
+	interface getBegin {
+
+	}
+
+	interface getEnd {
+
+	}
+
+	interface getAge {
+
+	}
+
+	interface getIBAN {
+
+	}
+
+	interface getAmount {
+
+	}
+
+	interface getBankPayment {
+
+	}
+
+	interface getRoomBooking {
+
+	}
+
+	interface getActivityBooking {
+
+	}
+
+	interface processPayment {
+
+	}
+
+	interface reserveHotel {
+
+	}
+
+	interface reserveActivity {
+
+	}
+
+	interface process {
+
+	}
+
+}
